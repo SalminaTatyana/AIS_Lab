e8cab89ea9005d28ed9bcef4c9797a5b671b329d
DESKTOP-BMCT3CB\79084 <Maxim300401@mail.ru>
2022-09-22 20:28:00 +0400

    Паршалы плюс контроллеры к ним

---
+++ AIS_Lab/AIS_Lab/Controllers/ViewsController/FreelanceСontroller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AIS_Lab.Controllers
+{
+    public class FreelanceСontroller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
